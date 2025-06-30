6ab829d0bdf04308fc1865d1325807763438d188
naoentreempanico <patricia.silvaxi@gmail.com>
2025-06-30 11:51:44 -0300

    Carrinho, cardapio e pizza.js atualizados, modal adicionado.

---
+++ pizza.js
@@ -0,0 +1,39 @@
+
+let cardapio = [{
+    foto: "img/pizza1.jpg",
+    nome:"Calabresa",
+    ingredientes: ["Calabresa", "Cabola", "Muçarela"],
+    valor: 35.00
+},
+{
+    foto: "img/pizza2.jpg",
+    nome: "Frango com Capury",
+    ingredientes: ["franco", "Capury", "Muçarela"],
+    valor: 40.00
+},
+{
+    foto: "img/pizza3.jpg",
+    nome: "Quatro queijos",
+    ingredientes: ["Muçarela", "Provolone", "Gorgonzola", "Parmesão"],
+    valor: 50.00
+
+},
+{
+    foto: "img/pizza4.jpg",
+    nome: "Bacon",
+    ingredientes: ["Qeuijo", "Bacon", "Milho", "Molho de tomate"],
+    valor: 50.00
+
+},
+{
+    foto: "img/pizza4.jpg",
+    nome: "Margarita",
+    ingredientes: ["Muçarela", "Mangericão", "Molho de tomate"],
+    valor: 45.00
+
+},
+
+
+
+
+];
